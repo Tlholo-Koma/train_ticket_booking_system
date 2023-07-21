d7c520f7e2f3ac675308f34f41abaccafe6fb10a
Raaga Krishna <raaga@bbd.co.za>
2023-07-21 14:24:11 +0200

    Added toString (for debugging).

--- backend/src/main/java/io/swagger/api/ApiResponseMessage.java
+++ backend/src/main/java/io/swagger/api/ApiResponseMessage.java
@@ -10,10 +10,10 @@ import java.time.Instant;
 
 @javax.xml.bind.annotation.XmlRootElement
 public class ApiResponseMessage {
+    Instant timestamp;
     int status;
     String type;
     String message;
-    Instant timestamp;
     Object data = null;
 
     public ApiResponseMessage(){}
@@ -77,5 +77,30 @@ public class ApiResponseMessage {
 
     public void setTimestamp(Instant timestamp) {
         this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class ApiResponseMessage {\n");
+
+        sb.append("    timestamp: ").append(toIndentedString(timestamp)).append("\n");
+        sb.append("    status: ").append(toIndentedString(status)).append("\n");
+        sb.append("    type: ").append(toIndentedString(type)).append("\n");
+        sb.append("    message: ").append(toIndentedString(message)).append("\n");
+        sb.append("    data: ").append(toIndentedString(data)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
     }
 }
