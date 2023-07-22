93c01fc518fe48a6535d95c08f29ee4901fee2c2
Raaga Krishna <raaga@bbd.co.za>
2023-07-22 20:09:39 +0200

    Fixed typo in Table name

--- backend/src/main/java/io/swagger/repository/TrainSeatRepository.java
+++ backend/src/main/java/io/swagger/repository/TrainSeatRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TrainSeatRepository extends JpaRepository<TrainSeat, Integer> {
 
-    @Query(value = "SELECT MAX(seat_id) FROM TrainSeat", nativeQuery = true)
+    @Query(value = "SELECT MAX(seat_id) FROM Seat", nativeQuery = true)
     Integer findMaxSeatId();
 }
