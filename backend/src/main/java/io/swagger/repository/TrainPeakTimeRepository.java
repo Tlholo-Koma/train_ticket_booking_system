4fb91b5476ae737e12de4c45aecc058ea8de912c
Joaquim-Chambuara <joaquim@bbd.co.za>
2023-07-25 12:41:39 +0200

    Merge branch 'jpastationsswag' of https://github.com/raagakrishna/train_ticket_booking_system.git into jpastationsswag

---
+++ backend/src/main/java/io/swagger/repository/TrainPeakTimeRepository.java
@@ -0,0 +1,10 @@
+package io.swagger.repository;
+
+import io.swagger.model.TrainPeakTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainPeakTimeRepository extends JpaRepository<TrainPeakTime, Integer> {
+   
+}
