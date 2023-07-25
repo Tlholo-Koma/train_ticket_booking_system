5c49c08ecbc0ab5edc2d02c8b4b0f13623a44d0d
Joaquim-Chambuara <122440707+Joaquim-Chambuara@users.noreply.github.com>
2023-07-25 19:36:55 +0200

    Update Passenger.java
    added seats to the passenger

--- backend/src/main/java/io/swagger/model/Passenger.java
+++ backend/src/main/java/io/swagger/model/Passenger.java
@@ -3,9 +3,15 @@ package io.swagger.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import io.swagger.utils.jackson.TrainSeatDeserializer;
 
 @Entity
 @Data
@@ -22,39 +28,37 @@ public class Passenger {
 	@JsonProperty("passenger_id")
 	private Integer passengerId;
 
-//	@JsonProperty("booking_id")
-//	@Column(name = "booking_id")
-//	private Integer bookingId;
-
-//	@JsonProperty("seat_id")
-//	@Column(name = "seat_id")
-//	private Integer seatId;
+	@JsonProperty("seat_id")
+	@Column(name = "seat_id")
+	private Integer seatId;
 
 	@JsonProperty("passenger_name")
 	@ToString.Include
 	@Column(name = "passenger_name", nullable = false)
 	private String passengerName;
-	
-//	@ManyToOne
-//	@JoinColumn(name = "booking_id", insertable = false, updatable = false)
-//	private UserBooking booking;
 
 	@JsonProperty("passenger_age")
 	@ToString.Include
 	@Column(name = "age")
 	private Integer age;
 
-
-//	@JsonProperty("date_created")
-//	@Column(name = "date_created", nullable = false)
-//	private LocalDate dateCreated;
-
-//	@JsonProperty("date_updated")
-//	@Column(name = "date_updated", nullable = false)
-//	private LocalDate dateUpdated;
-
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "booking_id")
 	@JsonIgnore
 	private Booking ticket;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "seat_id")
+	@JsonProperty("train_seats")
+	@JsonDeserialize(using = TrainSeatDeserializer.class)
+	private List<TrainSeat> seats;
+
+	@JsonProperty("date_created")
+	@Column(name = "date_created", nullable = false)
+	private LocalDate dateCreated;
+
+	@JsonProperty("date_updated")
+	@Column(name = "date_updated", nullable = false)
+	private LocalDate dateUpdated;
+
 }
