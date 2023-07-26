470188b4fa5b0bbf85e5a15f31797e1bcd58c25d
Tlholo Koma <tlholo@bbd.co.za>
2023-07-26 21:36:52 +0200

    add request structures

--- frontend/scripts/constant.js
+++ frontend/scripts/constant.js
@@ -1,1 +1,22 @@
 allaboard_url = "https://allaboard.bbdgrad.com/";
+
+async function apiGet(endpoint) {
+    return await fetch(allaboard_url+ endpoint, {
+    method: "GET",
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
+});
+}
+
+async function apiPost(endpoint, body) {
+    return await fetch(allaboard_url+ endpoint, {
+    method: "POST",
+    body: body,
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer ' + localStorage.getItem('token'),
+    }
+});
+}
