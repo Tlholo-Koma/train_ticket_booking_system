246fbadb0e062be43d127170efa13a9ff3a76877
Tlholo Koma <tlholo@bbd.co.za>
2023-07-26 11:50:11 +0200

    enable cors

--- backend/src/main/java/io/swagger/Main.java
+++ backend/src/main/java/io/swagger/Main.java
@@ -6,9 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -26,7 +28,19 @@ public class Main implements CommandLineRunner {
 
     public static void main(String[] args) throws Exception {
         new SpringApplication(Main.class).run(args);
+
+        
     }
+    
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 
     @Configuration
     static class MyConfig implements WebMvcConfigurer {
