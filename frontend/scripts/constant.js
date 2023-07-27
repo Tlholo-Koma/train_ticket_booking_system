0e470cda9d30447a7d4d9489ada298b0218d6877
Tlholo Koma <tlholo@bbd.co.za>
2023-07-27 16:45:46 +0200

    fixes

--- frontend/scripts/constant.js
+++ frontend/scripts/constant.js
@@ -1,8 +1,9 @@
-allaboard_url = "http://localhost:8080";
+allaboard_url = "https://allaboard.bbdgrad.com";
 
 async function apiGet(endpoint) {
     return await fetch(allaboard_url+ endpoint, {
     method: "GET",
+    credentials: "true",
     headers: {
         'Content-type': 'application/json; charset=UTF-8',
         'Authorization': 'Bearer ' + localStorage.getItem('token')
@@ -14,6 +15,7 @@ async function apiPost(endpoint, body) {
     return await fetch(allaboard_url+ endpoint, {
     method: "POST",
     body: body,
+    credentials: "true",
     headers: {
         'Content-type': 'application/json; charset=UTF-8',
         'Authorization': 'Bearer ' + localStorage.getItem('token'),
